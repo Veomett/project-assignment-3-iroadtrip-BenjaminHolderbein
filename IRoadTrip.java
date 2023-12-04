7bd1b05e3ec64e041c4df99f2656a05272aa1d0f
BenjaminHolderbein <bgholderbein@dons.usfca.edu>
2023-12-04 13:41:50 -0800

    Getting started

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,10 +1,15 @@
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class IRoadTrip {
+
+    HashMap<String, HashMap<String, Integer>> graph;
 
 
     public IRoadTrip (String [] args) {
         // Replace with your code
+        graph = new HashMap<>();
+        // generate the map by running a bunch of methods
     }
 
 
@@ -25,11 +30,50 @@ public class IRoadTrip {
         System.out.println("IRoadTrip - skeleton");
     }
 
+    public static String[] extractStateName(String filePath) {
+        ArrayList<String> countriesList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            // Read each line from the TSV file
+            while ((line = br.readLine()) != null) {
+                // Split the line using tab as a delimiter
+                String[] data = line.split("\t");
+
+                // Extract everything before the comma, exclude anything in parentheses,
+                // and exclude the part after the slash
+                if (data.length == 5 && data[4].equals("2020-12-31")) {
+                    String[] countryParts = data[2].split(",");
+                    String countryName = countryParts[0].replaceAll("\\(.*?\\)", "").split("/")[0].trim();
+                    // Add the extracted country name to the list
+                    countriesList.add(countryName);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Convert the list to an array
+        String[] countryArray = new String[countriesList.size()];
+        countryArray = countriesList.toArray(countryArray);
+
+        return countryArray;
+    }
 
     public static void main(String[] args) {
         IRoadTrip a3 = new IRoadTrip(args);
 
         a3.acceptUserInput();
+
+        String filePath = "state_name.tsv";
+        String[] countryArray = extractStateName(filePath);
+
+        // Print the extracted country names
+        System.out.println("Countries with end date as 2020-12-31:");
+        for (String country : countryArray) {
+            System.out.println(country);
+        }
     }
 
 }
